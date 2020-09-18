cfbe85f9e1121c38ab80d51304ac9f4ac3e2d010
chaoranwang <270790167@qq.com>
2020-09-18 10:43:51 +0800

    here is the another change~

---
+++ efasjfiuasj.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/10 6:04 下午
+# @Author  : Chaorn Wang
+# @Email   : chaoran.wcr@dtwave-inc.com
+# @File    : efasjfiuasj.py
+print("the new test file of python")
